fee9c1876c3e786bc922b2e866c65e4d975e3ce4
Carlos Arroyo <fm7ybfjr4w@privaterelay.appleid.com>
2022-12-21 06:57:26 +0000

    c

--- Ejercicios SELECT/Select.sql
+++ Ejercicios SELECT/Select.sql
@@ -12,6 +12,11 @@ SELECT * FROM EMPLOYEES;
 SELECT TO_NUMBER(ROUND(MAX(e.salary)),'$99,999.99') AS "Salario Máximo",
        TO_NUMBER(ROUND(MIN(e.salary)),'$99,999.99') AS "Salario Mínimo",
        TO_NUMBER(ROUND(SUM(e.salary)),'$99,999.99')AS "Salario Total",
-       TO_NUMBER(ROUND(AVG(e.salary)),'$99,999.99') AS "Salario medio"
-FROM Employees e
+TO_NUMBER(ROUND(AVG(e.salary)),'$999,999.99') AS "Salario medio"
+FROM Employees e;
+
+
+
+
+/*Mostrarel salario más alto, más bajo, salario total y salario promedio por cada tipo de puesto que se  tiene  en  la organización.  Etiquete  las  columnas  como  «Puesto»,  «Salario  Máximo»,  «Salario Mínimo», «Salario Total» y «Salario Promedio» respectivamente. Redondee los resultados con dos cifras decimales, con un formato numérico para la moneda de dólares*/
 
